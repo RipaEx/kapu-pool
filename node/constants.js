7a1829284b2061e2809c44ac825ded5fc806591e
Giovanni Silvestri <gsit80@gmail.com>
2019-03-31 16:38:37 +0200

    Pool sharing: 03/31/2019

--- node/constants.js
+++ node/constants.js
@@ -1,12 +1,12 @@
-const MAIN_NET_ENDPOINT = 'https://api2.ripaex.io'; //'http://51.15.198.173:4600';
+const MAIN_NET_ENDPOINT = 'https://api.ripaex.io'; //'http://51.15.198.173:4600';
 const MAIN_NET_NETHASH = 'bee1634649fc6a759e5fdb8f3c4bcb4b5189c1f2a6b48284a6445f3f09db844e';
 const DEV_NET_ENDPOINT = '';//'http://51.15.67.153:4100'
 const DEV_NET_NETHASH = '';
-const GET_NET_HASH_ENDPOINT = '/api/blocks/getNetHash';
-const TRANSACTIONS_ENDPOINT = '/peer/transactions';
+const GET_NET_HASH_ENDPOINT = '/api/blocks/getNethash';
+const TRANSACTIONS_ENDPOINT = '/api/v2/transactions';
 const HEADER_CONTENT_TYPE = 'application/json';
 const HEADER_OS = 'ripa-pool';
-const HEADER_PORT = 5500;
+const HEADER_PORT = 5502;
 const HEADER_VERSION = require('./package.json').version;
 const PRECISION = 8;
 const MESSAGE_1 = 'pool sharing - 95%: thank You for your vote';
